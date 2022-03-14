dfe7eb3451d762352a96a637d59cfb08246068ff
Redomine <52177952+Redomine@users.noreply.github.com>
2022-03-14 17:10:55 +0300

    Доработан расчет тройников

--- HVAC_MRA/SP_HVAC.extension/ОВиК_MRA.tab/Расчет КМС.panel/Расчет КМС.pushbutton/script.py
+++ HVAC_MRA/SP_HVAC.extension/ОВиК_MRA.tab/Расчет КМС.panel/Расчет КМС.pushbutton/script.py
@@ -104,7 +104,6 @@ def getDpTransition(element):
             
     return dp
 
-
 def getConCoords(connector):
     a0 = connector.Origin.ToString()
     a0 = a0.replace("(", "")
@@ -114,83 +113,173 @@ def getConCoords(connector):
         x = float(x)
     return a0
 
+def getDuctCoords(inTeeCon, connector):
+    mainCon = []
+    connectorSet = connector.AllRefs.ForwardIterator()
+    while connectorSet.MoveNext():
+        mainCon.append(connectorSet.Current)
+    duct = mainCon[0].Owner
+    ductCons = getConnectors(duct)
+    for ductCon in ductCons:
+        if getConCoords(ductCon) != inTeeCon:
+            inDuctCon = getConCoords(ductCon)
+            return inDuctCon
+
+
+
 def getTeeOrient(element):
     connectors = getConnectors(element)
-    orient = 1
-    connector_out = []
+    exitCons = []
+    exhaustAirCons = []
     for connector in connectors:
-        if str(connector.Direction) == "Out":
-            connector_out.append(connector)
+        if str(connectors[0].DuctSystemType) == "SupplyAir":
+            if connector.Flow != max(connectors[0].Flow, connectors[1].Flow, connectors[2].Flow):
+                exitCons.append(connector)
+        if str(connectors[0].DuctSystemType) == "ExhaustAir":
+            #а что делать если на на разветвлении расход одинаковы?
+            if connector.Flow == max(connectors[0].Flow, connectors[1].Flow, connectors[2].Flow):
+                exitCons.append(connector)
+            else:
+                exhaustAirCons.append(connector)
+            #для входа в тройник ищем координаты начала входящего воздуховода чтоб построить прямую через эти две точки
 
-        #для входа в тройник ищем координаты начала входящего воздуховода чтоб построить прямую через эти две точки
-        connector_p = []
-        if str(connector.Direction) == "In":
-            inTee = getConCoords(connector)
-            a = connector.AllRefs.ForwardIterator()
-            while a.MoveNext():
-                connector_p.append(a.Current)
-            duct = connector_p[0].Owner
-            duct_connectors = getConnectors(duct)
-            for duct_connector in duct_connectors:
-                if getConCoords(duct_connector) != inTee:
-                    inDuct = getConCoords(duct_connector)
+        if str(connectors[0].DuctSystemType) == "SupplyAir":
+            if str(connector.Direction) == "In":
+                inTeeCon = getConCoords(connector)
+                # выбираем из коннектора подключенный воздуховод
+                inDuctCon = getDuctCoords(inTeeCon, connector)
 
+    #в случе вытяжной системы, чтоб выбрать коннектор с выходящим воздухом из второстепенных, берем два коннектора у которых расход не максимальны
+    #(максимальный точно выходной у вытяжной системы) и сравниваем. Тот что самый малый - ответветвление
+    #а второй - точка вхождения потока воздуха из которой берем координаты для построения вектора
+    if str(connectors[0].DuctSystemType) == "ExhaustAir":
+
+        if exhaustAirCons[0].Flow < exhaustAirCons[1].Flow:
+            exitCons.append(exhaustAirCons[0])
+            inTeeCon = getConCoords(exhaustAirCons[1])
+            inDuctCon = getDuctCoords(inTeeCon, exhaustAirCons[1])
+        else:
+            exitCons.append(exhaustAirCons[1])
+            inTeeCon = getConCoords(exhaustAirCons[0])
+            inDuctCon = getDuctCoords(inTeeCon, exhaustAirCons[0])
 
     # среди выходящих коннекторов ищем диктующий по большему расходу
     try:
-        if connector_out[0].Flow > connector_out[1].Flow:
-            connector_o = connector_out[0]
+        if exitCons[0].Flow > exitCons[1].Flow:
+            exitCon = exitCons[0]
+            secondaryCon = exitCons[1]
         else:
-            connector_o = connector_out[1]
+            exitCon = exitCons[1]
+            secondaryCon = exitCons[0]
     except Exception:
         print element.Id
-        print connector_out
-    connector_o = getConCoords(connector_o)
+        print exitCons
+
+    #диктующий коннектор
+    exitCon = getConCoords(exitCon)
+
+    #вторичный коннектор
+    secondaryCon = getConCoords(secondaryCon)
 
     #найдем вектор по координатам точек AB = {Bx - Ax; By - Ay; Bz - Az}
-    Tee_Duct = [(float(inDuct[0]) - float(inTee[0])), (float(inDuct[1]) - float(inTee[1])), (float(inDuct[2]) - float(inTee[2]))]
-    Tee_Out = [(float(inTee[0]) - float(connector_o[0])), (float(inTee[1]) - float(connector_o[1])), (float(inTee[2]) - float(connector_o[2]))]
+    ductToTee = [(float(inDuctCon[0]) - float(inTeeCon[0])), (float(inDuctCon[1]) - float(inTeeCon[1])),
+                 (float(inDuctCon[2]) - float(inTeeCon[2]))]
+
+    teeToExit = [(float(inTeeCon[0]) - float(exitCon[0])), (float(inTeeCon[1]) - float(exitCon[1])),
+               (float(inTeeCon[2]) - float(exitCon[2]))]
+
+    #то же самое для вторичного отвода
+    teeToMinor = [(float(inTeeCon[0]) - float(secondaryCon[0])), (float(inTeeCon[1]) - float(secondaryCon[1])),
+               (float(inTeeCon[2]) - float(secondaryCon[2]))]
 
     #найдем скалярное произведение векторов AB · CD = ABx · CDx + ABy · CDy + ABz · CDz
-    Tee_Out_Tee_Duct = Tee_Duct[0]*Tee_Out[0] + Tee_Duct[1]*Tee_Out[1] + Tee_Duct[2]*Tee_Out[2]
+    teeToExit_ductToTee = ductToTee[0]*teeToExit[0] + ductToTee[1]*teeToExit[1] + ductToTee[2]*teeToExit[2]
+
+    #то же самое с вторичным коннектором
+    teeToMinor_ductToTee = ductToTee[0]*teeToMinor[0] + ductToTee[1]*teeToMinor[1] + ductToTee[2]*teeToMinor[2]
 
     #найдем длины векторов
-    len_Tee_Duct = ((Tee_Duct[0])**2 + (Tee_Duct[1])**2 + (Tee_Duct[2])**2)**0.5
-    len_Tee_Out = ((Tee_Out[0])**2 + (Tee_Out[1])**2 + (Tee_Out[2])**2)**0.5
+    len_ductToTee = ((ductToTee[0])**2 + (ductToTee[1])**2 + (ductToTee[2])**2)**0.5
+    len_teeToExit = ((teeToExit[0])**2 + (teeToExit[1])**2 + (teeToExit[2])**2)**0.5
+
+    #то же самое для вторичного вектора
+    len_teeToMinor = ((teeToMinor[0])**2 + (teeToMinor[1])**2 + (teeToMinor[2])**2)**0.5
+
 
     #найдем косинус
-    cos = (Tee_Out_Tee_Duct)/(len_Tee_Duct * len_Tee_Out)
+    cosMain = (teeToExit_ductToTee)/(len_ductToTee * len_teeToExit)
+
+    #то же самое с вторичным вектором
+    cosMinor = (teeToMinor_ductToTee) / (len_ductToTee * len_teeToMinor)
 
     #Если угол расхождения между вектором входа воздуха и выхода больше 10 градусов(цифра с потолка) то считаем что идет буквой L
     #Если нет, то считаем что идет по прямой буквой I
-    if math.acos(cos) > 0.10:
-        orient = 'L'
+
+    #тип 1
+    #подающий воздуховод, dp = (0.4408 * (v2/v1)**2 - 0.7619 * v2/v1 + 0.3785) * 0.6 * v1 ** 2
+    if math.acos(cosMain) < 0.10 and str(connectors[0].DuctSystemType) == "SupplyAir":
+        type = 1
+
+    #тип 2
+    #вытяжной воздуховод, dp = 0.2 * (v2/v1)**-0.76 * 0.6 * v1**2
+    elif math.acos(cosMain) < 0.10 and str(connectors[0].DuctSystemType) == "ExhaustAir":
+        type = 2
+
+    #тип 3
+    #вытяжной воздуховод, dp = (0.7 * (v2/v1)**2 + 0.4 * v2/v1 - 0.4)*0.6*v1**2
+    elif math.acos(cosMain) > 0.10 and str(connectors[0].DuctSystemType) == "ExhaustAir":
+        type = 3
+
+    #тип 4
+    #подающий воздуховод, dp = (0.4 * v2/v1 + 1) * 0.6 * v1 ** 2
+    elif math.acos(cosMain) > 0.10 and str(connectors[0].DuctSystemType) == "SupplyAir":
+        type = 4
+
     else:
-        orient = 'I'
-    return orient
+        type = 5
+
+    return type
 
 
 def getDpTee(element):
-    a = getConnectors(element)
-
-    getTeeOrient(element)
+    conSet = getConnectors(element)
+    type = getTeeOrient(element)
     dp = 10
-    # try:
-    #     S1 = a[1].Height*0.3048*a[1].Width*0.3048
-    # except:
-    #     S1 = 3.14*0.3048*0.3048*a1.Radius**2
-    # try:
-    #     S2 = a2.Height*0.3048*a2.Width*0.3048
-    # except:
-    #     S2 = 3.14*0.3048*0.3048*a2.Radius**2
-    # try:
-    #     S3 = a3.Height*0.3048*a3.Width*0.3048
-    # except:
-    #     S3 = 3.14*0.3048*0.3048*a3.Radius**2
-    #
-    # v1 = a1.Flow*101.94/3600/S1
-    # v2 = a2.Flow*101.94/3600/S2
-    # v3 = a3.Flow*101.94/3600/S3
+    try:
+        S1 = conSet[0].Height*0.3048*conSet[0].Width*0.3048
+    except:
+        S1 = 3.14*0.3048*0.3048*conSet[0].Radius**2
+    try:
+        S2 = conSet[1].Height*0.3048*conSet[1].Width*0.3048
+    except:
+        S2 = 3.14*0.3048*0.3048*conSet[1].Radius**2
+    try:
+        S3 = conSet[2].Height*0.3048*conSet[2].Width*0.3048
+    except:
+        S3 = 3.14*0.3048*0.3048*conSet[2].Radius**2
+
+    v1 = conSet[0].Flow*101.94/3600/S1
+    v2 = conSet[1].Flow*101.94/3600/S2
+    v3 = conSet[2].Flow*101.94/3600/S3
+
+    Vset = [v1, v2, v3]
+    V1 = max(Vset)
+    Vset.remove(V1)
+    if Vset[0] > Vset[1]:
+        V2 = Vset[0]
+    else:
+        V2 = Vset[1]
+
+    if type == 1:
+        dp = (0.4408 * (V2 / V1) ** 2 - 0.7619 * V2 / V1 + 0.3785) * 0.6 * V1 ** 2
+    if type == 2:
+        dp = 0.2 * (V2 / V1) ** -0.76 * 0.6 * V1 ** 2
+    if type == 3:
+        dp = (0.7 * (V2 / V1) ** 2 + 0.4 * V2 / V1 - 0.4) * 0.6 * V1 ** 2
+    if type == 4:
+        dp = (0.4 * V2/V1 + 1) * 0.6 * V1 ** 2
+    if type == 5:
+        dp = (0.56 * V2/V1 + 0.6)*0.6*V1**2
     return dp
         
 def getLossMethods(serviceId):
@@ -213,8 +302,7 @@ def getServerById(serverGUID, serviceId):
         server = service.GetServer(serverGUID)
         if server != "null":
             return server
-    return null
-
+    return None
 
 elems=colFittings
 
